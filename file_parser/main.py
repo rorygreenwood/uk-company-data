837fb9c6930e4fadd06eef08d76f63beb9d0c31d
T199811E <rory.greenwood@iqblade.cloud>
2023-01-19 15:11:45 +0000

    added pipeline message

--- file_parser/main.py
+++ file_parser/main.py
@@ -3,9 +3,15 @@ import shutil
 import time
 import mysql.connector
 import os
+import logging
 from file_downloader.companyhouse_transfer import collect_companieshouse_file
-from utils import unzip_ch_file, fragment_ch_file
+from utils import unzip_ch_file, fragment_ch_file, pipeline_messenger
 from fragment_work import parse_fragment, load_fragment
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s [line:%(lineno)d] %(levelname)s: %(message)s')
+
 
 db = mysql.connector.connect(
     host=os.environ.get('PREPRODHOST'),
@@ -17,22 +23,27 @@ schema = 'iqblade'
 cursor = db.cursor()
 t_fragment_file = 'BasicCompanyDataAsOneFile-2023-01-01.csv'
 # download file
-print('downloading file')
+logging.info('downloading file')
 ch_file, ch_upload_date = collect_companieshouse_file()
 str_ch_file = str(ch_file)
-print('unzipping file')
+logging.info('unzipping file')
 unzipped_ch_file = unzip_ch_file(ch_file)
 fragment_ch_file(f'../file_downloader/files/{unzipped_ch_file}')
 fragment_list = os.listdir('../file_downloader/files/fragments/')
 os.remove(f'../file_downloader/files/{unzipped_ch_file}')
 for fragment in fragment_list:
-    print(fragment)
+    logging.info(fragment)
     parse_fragment(f'../file_downloader/files/fragments/{fragment}')
     load_fragment(cursor, db)
-    print('------')
+    logging.info('------')
     os.remove(f'../file_downloader/files/fragments/{fragment}')
 
 # when done, update filetracker
 filetracker_tup = (str_ch_file, ch_upload_date, datetime.datetime.now(), datetime.datetime.now())
 cursor.execute("""insert into BasicCompanyData_filetracker (filename, ch_upload_date, lastDownloaded, lastProcessed) VALUES (%s, %s, %s, %s)""", filetracker_tup)
 db.commit()
+
+pipeline_title = 'Companies House File loaded'
+pipeline_message = f'File Date: {ch_upload_date}'
+pipeline_hexcolour = '#4798e2'
+pipeline_messenger(title=pipeline_title, text=pipeline_message, hexcolour=pipeline_hexcolour)
--- file_parser/utils.py
+++ file_parser/utils.py
@@ -1,7 +1,8 @@
 import logging
 import os
 import zipfile
-
+import requests
+import json
 from filesplit.split import Split
 
 
@@ -18,3 +19,18 @@ def fragment_ch_file(file_name):
     split = Split(file_name, '../file_downloader/files/fragments/')
     split.bylinecount(linecount=50000, includeheader=True)
     os.remove('../file_downloader/files/fragments/manifest')
+
+
+def pipeline_messenger(title, text, hexcolour):
+    url = "https://tdworldwide.webhook.office.com/webhookb2/d5d1f4d1-2858-48a6-8156-5abf78a31f9b@7fe14ab6-8f5d-4139-84bf-cd8aed0ee6b9/IncomingWebhook/76b5bd9cd81946338da47e0349ba909d/c5995f3f-7ce7-4f13-8dba-0b4a7fc2c546"
+    payload = json.dumps({
+        "@type": "MessageCard",
+        "themeColor": hexcolour,
+        "title": title,
+        "text": text,
+        "markdown": True
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    requests.request("POST", url, headers=headers, data=payload)
