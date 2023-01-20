4577654ce5afe54b82d3da332f32d3eb1d777c81
T199811E <rory.greenwood@iqblade.cloud>
2023-01-20 12:41:31 +0000

    changed cmd to include directory

--- file_downloader/companyhouse_transfer.py
+++ file_downloader/companyhouse_transfer.py
@@ -14,7 +14,7 @@ def collect_companieshouse_file(firstDayOfMonth):
     baseurl = 'http://download.companieshouse.gov.uk/' + filename
     logger.info(f'sending request using url: {baseurl}')
     req = r.get(baseurl, stream=True)
-    with open('/file_downloader/files/' + filename, 'wb') as fd:
+    with open('file_downloader/files/' + filename, 'wb') as fd:
         chunkcount = 0
         for chunk in req.iter_content(chunk_size=100000):
             chunkcount += 1
