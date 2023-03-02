a66dbb65119e20c7296e68b38aa86cdcd12d944e
T199811E <rory.greenwood@iqblade.cloud>
2023-03-02 14:59:12 +0000

    removed old files

--- post_insert_updates_active.py
+++
@@ -1,9 +0,0 @@
-
-def post_update_activity(cursor, db):
-    cursor.execute("""
-    update organisation o
-    inner join raw_companies_house_input_stage rchis
-    on o.id = rchis.organisation_id
-    set o.company_status = rchis.company_status""")
-
-    db.commit()
